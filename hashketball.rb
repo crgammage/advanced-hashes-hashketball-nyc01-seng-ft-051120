e1c29ee9bd0a94313d27df4efb03804fccc0c531
Christa Gammage <crgammage@gmail.com>
2020-04-27 21:40:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -199,7 +199,7 @@ def big_shoe_rebounds
     team_details[:players].each do |player|
       if player[:shoe] > big_shoe
         big_shoe = player[:shoe]
-        rebounds =player[:rebounds]
+        rebounds = player[:rebounds]
       end
     end
   end
